1f3634ef418f023d7162c2298dc79005c65a64ec
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2021-12-22 17:43:58 +0800

    Add WeekList component

---
+++ components/WeekList.js
@@ -0,0 +1,31 @@
+const weeks = 12;
+export function Week({ number }) {
+  return (
+    <button className="text-blueGray-500 bg-transparent border border-solid border-blueGray-500 hover:bg-blueGray-500 hover:text-white active:bg-blueGray-600 font-bold uppercase text-sm px-6 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+      { number }
+    </button>
+  );
+};
+
+
+export default function WeekList({ weekLength }) {
+  const arr = [];
+
+  for (let i = 1; i <= weekLength; i ++) {
+    arr.push(<Week number={`${i}`} />);
+  }
+  
+  return (
+    <>
+      <div className="relative bg-blueGray-800 md:pt-32 pb-32 pt-12">
+        <div className="px-4 md:px-10 mx-auto w-full">
+          <div>
+            <div className="flex flex-wrap">
+              {arr}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/training.js
+++ pages/training.js
@@ -3,7 +3,7 @@ import AdminNavbar from '../components/Navbars/AdminNavbar';
 import FooterAdmin from '../components/Footers/FooterAdmin';
 import Sidebar from '../components/Sidebar/sidebar';
 import CardTable from '../components/Cards/CardTable';
-import HeaderStats from '../components/Headers/HeaderStats';
+import WeekList from '../components/WeekList';
 
 export default function Training() {
   return(
@@ -18,7 +18,7 @@ export default function Training() {
       <Sidebar />
       <div className="relative md:ml-64 bg-blueGray-100">
         <AdminNavbar />
-        <HeaderStats />
+        <WeekList weekLength={12} />
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
           <div className="flex flex-wrap mt-4">
             <div className="w-full mb-12 px-4">
