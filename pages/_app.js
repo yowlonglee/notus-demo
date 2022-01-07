87f8cabeb55e93439c9c07aac385e1d069ffbf45
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2022-01-07 09:11:49 +0800

    Add Apollo Client

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,24 @@
+import { ApolloProvider } from '@apollo/client';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import withData from '../lib/withData';
 import '../styles/tailwind.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export function App({ Component, pageProps, apollo }) {
+  console.log(apollo);
+  return (
+    <ApolloProvider client={apollo}>
+      <Component {...pageProps} />;
+    </ApolloProvider>
+  );
 }
+
+App.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(App);
