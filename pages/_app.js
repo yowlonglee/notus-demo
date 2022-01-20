e099ed08c15c8c0048fa294ed291ea0f9b64fb26
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2022-01-20 16:41:43 +0800

    Add NProgress package

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import { ApolloProvider } from '@apollo/client';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import withData from '../lib/withData';
 import '../styles/tailwind.css';
+
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export function App({ Component, pageProps, apollo }) {
   return (
