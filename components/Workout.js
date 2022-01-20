e60abbcd50aacf3fa9a40a8684b738e081a39ac3
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2022-01-20 10:52:45 +0800

    Update workout layout on calendar page

--- components/Workout.js
+++ components/Workout.js
@@ -6,11 +6,22 @@ export default function Workout({ workout }) {
   return (
     <div className="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 shadow-sm">
       <div className="flex-auto p-4">
-        <p>{day}</p>
-        <p>{`${date.getMonth() + 1}月${date.getDate()}日`}</p>
-        <p>{workout.title}</p>
-        <p className="whitespace-pre">{workout.detail}</p>
-        <p>{workout.user.name}</p>
+        <div className="flex flex-wrap">
+          <div className="relative pr-4 flex-initial flex flex-col">
+            <span className="text-blueGray-400 font-bold text-sm">{`${
+              date.getMonth() + 1
+            }月${date.getDate()}日`}</span>
+            <span className="font-semibold text-blueGray-700">{day}</span>
+          </div>
+          <div className="flex-initial">
+            <h5 className="text-xl font-semibold inline-block p-2 rounded text-orange-600 bg-orange-200">
+              {workout.title}
+            </h5>
+          </div>
+        </div>
+        <p className="whitespace-pre mt-4 text-blueGray-500">
+          {workout.detail}
+        </p>
       </div>
     </div>
   );
