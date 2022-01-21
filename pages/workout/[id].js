40a28f3f4f431d54e02872f8f64aa87ddc36f88e
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2022-01-21 14:38:55 +0800

    Add SingleWorkout component

--- pages/workout/[id].js
+++ pages/workout/[id].js
@@ -1,53 +1,17 @@
-import { useQuery } from '@apollo/client';
-import gql from 'graphql-tag';
 import Head from 'next/head';
 import Layout from '../../components/Layout';
+import SingleWorkout from '../../components/SingleWorkout';
 
-const SINGLE_WORKOUT_QUERY = gql`
-  query SINGLE_WORKOUT_QUERY($id: ID!) {
-    Workout(where: { id: $id }) {
-      title
-      user {
-        name
-        id
-      }
-      type
-      date
-      detail
-      status
-      feels
-      comment
-      image {
-        id
-        image {
-          id
-          path
-        }
-        description
-      }
-    }
-  }
-`;
-
-export default function WorkoutPage({ query }) {
-  const { data, error, loading } = useQuery(SINGLE_WORKOUT_QUERY, {
-    variables: { id: query.id },
-  });
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-  const workout = data.Workout;
-  console.log(workout);
+export default function SingleWorkoutPage({ query }) {
   return (
     <Layout>
       <Head>
-        <title>KRC - {workout.title}</title>
+        {/* <title>KRC - {workout.title}</title> */}
+        <title>KRC</title>
       </Head>
       <div className="flex flex-wrap">
         <div className="w-full mb-12 px-4">
-          <div className="relative bg-blueGray-100">
-            This is a single workout page!
-          </div>
-          <div>ID: {query.id}</div>
+          <SingleWorkout id={query.id} />
         </div>
       </div>
     </Layout>
